b953e2cd3eae8e976eac35ceebe521170a96249b
linkpurpleteam <linkpurpleteam@gmail.com>
2023-11-15 17:13:04 -0500

    Ajuste2

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/login-prestador',
-        'login',
-        'register-prestador',
-        'register'
+        
     ];
 }
