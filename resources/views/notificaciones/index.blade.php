90f3aa4bced497ba5fbfd832f87a09be423c561d
Pracma16 <oportunilan@gmail.com>
2023-11-23 17:30:13 -0300

    Estilar notificaciones index , agregar usuario.css

--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -1,28 +1,54 @@
+@endsection
+
 @extends('layouts.prestador-layout')
 
 @section('contenido-prestador')
-<div class="container-contacto">
-    <h2>
-        Mis Notificaciones
-    </h2>
-    @if (count($notificaciones ) > 0)
+
+<article class="publicacion-servicio">
+    <div class="titulo-publicar">
+        <h1>Mis notificaciones</h1>
+        <livewire:crear-oferta />
+
+        @if (count($notificaciones ) > 0)
     @foreach ($notificaciones as $notificacion)
-        <div>
-            <p>Tienes una nueva solicitud:
-                <span>{{$notificacion->data['nombre_oferta']}}</span>
-            </p>
-            <p>Hace:
-                <span>{{$notificacion->created_at->diffForHumans()}}</span>
-            </p>
-        </div>
-        <div>
-            <a href="#">Ver Solicitantes</a>
-        </div>
+
+    </div>
+    <div class="conten-margen-usuario">
+        <div class="conten-usuario">
+            <div class="miperfil-usuario-notificacion">
+                <div class="title-perfil">
+                    <h4>Solicitud de servicio</h4>
+                </div>
+                <p class="dato-usuario">Notificacion:</p>
+                <div>
+                    <p>Tienes una nueva solicitud:
+                        <span>{{$notificacion->data['nombre_oferta']}}</span>
+                    </p>
+                </div>
+                <p class="dato-usuario">Tiempo:</p>
+                <div>
+                    <p>Hace:
+                        <span>{{$notificacion->created_at->diffForHumans()}}</span>
+                    </p>
+                </div>
+                <p class="dato-usuario">Telefono:</p>
+                <div>
+                    <p>{{auth()->user()->Telefono}}</p>
+                </div>
+            </div> 
+            <div class="ver-notificacion">
+                <div class="btn-contacto">
+                    <a href="#" >Ver</a>
+                </div>  
+            </div>
+        </div>    
+    </div>
+
     @endforeach
     @else
-        <p>no tienes notificaciones</p>
-    @endif
-    
-</div>
-    
+        <p style="border: 2px solid var(--blanco); padding: 1%; background-color: var(--naranja); color: var(--blanco); ">No tienes notificaciones</p>
+    @endif  
+
+</article>
+
 @endsection
