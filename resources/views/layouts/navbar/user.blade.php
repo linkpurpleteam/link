11a37a23131399279b937c81abb1cdfb2a67eede
Pracma16 <oportunilan@gmail.com>
2023-11-22 21:07:18 -0300

    Estilo navbar user -editar-ofertas y mostrar-ofertas

--- resources/views/layouts/navbar/user.blade.php
+++ resources/views/layouts/navbar/user.blade.php
@@ -1,18 +1,18 @@
 <header class="header">
     <nav class="navbar bg-body-tertiary">
-        <img id="logo" src="{{asset('imagenes/logo.png')}}" alt="Logo" width="75" height="40" class="d-inline-block align-text-top">
+        <img id="logo" src="{{route('imagenes/logo y letra.png')}}" alt="Logo" width="200vh" height="200vh" class="d-inline-block align-text-top">
             <div class="container-fluid">
-              <a href="{{route('post-prestador.index')}}" class="active d-none d-lg-block">Mis Perfil</a>
-              <a href="{{route('oferta.create')}}" class="active d-none d-lg-block">Solicitudes</a>
-              @auth
-              <a class="active d-none d-lg-block">
-              <form method="POST" action="{{route('logout')}}">
-                  @csrf
-                  <button type="submit">Cerrar Sesión</button>
-              </form>
-              </a>
-              @endauth
-              @guest
-              <a href="/login"><button class="btn btn-primary ml-auto active d-none d-lg-block">Iniciar Sesión</button></a>
-              @endguest
+            <a href="{{route('post-prestador.index')}}" class="active d-none d-lg-block"><img style="width: 25%; height: auto;" src="{{route('imagenes/logomiperfil.png')}}" alt="logomiperfil">&#160;Mi perfil</a>
+            <a href="{{route('oferta.create')}}" class="active d-none d-lg-block">Solicitudes</a>
+            @auth
+            <a class="active d-none d-lg-block">
+            <form method="POST" action="{{route('logout')}}">
+                @csrf
+                <button class="btn ml-auto active d-none d-lg-block" style="color:white; border-radius: 13px; border: 1px solid white; background-color: #2eb4b4;" type="submit">Cerrar Sesión</button>
+            </form>
+            </a>
+            @endauth
+            @guest
+            <a href="/login"><button class="btn ml-auto active d-none d-lg-block" style="color:white; border-radius: 13px; border: 1px solid white; background-color: #2eb4b4;" type="submit">Iniciar Sesión</button></a>
+            @endguest
 </header>
--- resources/views/livewire/editar-ofertas.blade.php
+++ resources/views/livewire/editar-ofertas.blade.php
@@ -1,73 +1,81 @@
-<form class="form-contacto" wire:submit.prevent='editarVacante'>
-    <div>
-        <label for="titulo">Titulo Oferta</label>
-        <input id="titulo"
-            name="titulo"
-            type="text"
-            wire:model="titulo"
-            placeholder="Titulo Oferta">
-            @error('titulo')
-            {{$message}}
+<section class="contenedor-crear-oferta">
+    <div class="titulo-publicar">
+        <h1>Editar publicacion</h1>
+    </div>
+    <div class="contenido-formulario-publi">
+    <form class="form-publicacion" wire:submit.prevent='editarVacante'>
+        <div>
+            <label for="titulo">Titulo Oferta</label>
+            <input id="titulo"
+                name="titulo"
+                type="text"
+                wire:model="titulo"
+                placeholder="Titulo Oferta">
+                @error('titulo')
+                {{$message}}
+                @enderror
+        </div>
+        
+        <div>
+            <label for="precio">Precio por Hora</label>
+            <select name="precio" id="precio" wire:model="precio">
+                <option>-- Seleccione --</option>
+                @foreach ($precios as $precio)
+                    <option value="{{$precio->id}}">{{$precio->precio}}</option>
+                @endforeach
+            </select>
+            @error('precio')
+                {{$message}}
             @enderror
-    </div>
-    
-    <div>
-        <label for="precio">Precio por Hora</label>
-        <select name="precio" id="precio" wire:model="precio">
-            <option>-- Seleccione --</option>
-            @foreach ($precios as $precio)
-                <option value="{{$precio->id}}">{{$precio->precio}}</option>
-            @endforeach
-        </select>
-        @error('precio')
-            {{$message}}
-        @enderror
-    </div>
-    <div>
-        <label for="categoria">Categoria</label>
-        <select name="categoria" id="categoria" wire:model="categoria">
-            <option>-- Seleccione --</option>
-            @foreach ($categorias as $categoria)
-                <option value="{{$categoria->id}}">{{$categoria->categoria}}</option>
-            @endforeach
-        </select>
-        @error('categoria')
-            {{$message}}
-        @enderror
-    </div>
-    <div>
-        <label for="descripcion">Descripcion de la Oferta</label>
-        <textarea name="descripcion" 
-        wire:model="descripcion"
-        id="descripcion"  
-        placeholder="Descripcion de la Oferta...">
-        </textarea>
-        @error('descripcion')
-            {{$message}}
-        @enderror
-    </div>
-    <div>
-        <label for="imagen">Imagen</label>
-        <input id="imagen"
-            name="imagen"
-            type="file"
-            wire:model="imagen_nueva"
-            accept="image/*">
-        <div class="imagen-container">
-          <label>Imagen Actual</label>
-            <img src="{{ asset('storage/ofertas/' . $imagen)  }}" alt="{{'Imagen Vacante' . $titulo}}">
         </div>
-        <div class="imagen-container">
-            @if ($imagen_nueva)
-                Imagen Nueva:
-                <img src="{{ $imagen_nueva->temporaryUrl() }}">
-            @endif
+        <div>
+            <label for="categoria">Categoria</label>
+            <select name="categoria" id="categoria" wire:model="categoria">
+                <option>-- Seleccione --</option>
+                @foreach ($categorias as $categoria)
+                    <option value="{{$categoria->id}}">{{$categoria->categoria}}</option>
+                @endforeach
+            </select>
+            @error('categoria')
+                {{$message}}
+            @enderror
         </div>
-        @error('imagen_nueva')
-            {{$message}}
-        @enderror
-    </div>
-    <button class="btn btn-contacto">
-        Editar
-    </button>
-</form>
+        <div class="descripcion-oferta">
+            <label for="descripcion">Descripcion de la Oferta</label>
+            <textarea name="descripcion" 
+            wire:model="descripcion"
+            id="descripcion"  
+            placeholder="Descripcion de la Oferta...">
+            </textarea>
+            @error('descripcion')
+                {{$message}}
+            @enderror
+        </div>
+        <div>
+            <label for="imagen">Imagen</label>
+            <input id="imagen"
+                name="imagen"
+                type="file"
+                wire:model="imagen_nueva"
+                accept="image/*">
+            <div class="cambio-imagen">
+                <div class="imagen-container">
+                <label>Imagen Actual</label>
+                    <img src="{{ asset('storage/ofertas/' . $imagen)  }}" alt="{{'Imagen Vacante' . $titulo}}">
+                </div>
+                <div class="imagen-container">
+                    @if ($imagen_nueva)
+                        Imagen Nueva:
+                        <img src="{{ $imagen_nueva->temporaryUrl() }}">
+                    @endif
+                </div>
+                @error('imagen_nueva')
+                    {{$message}}
+                @enderror
+            </div>  
+        </div>
+        <button class="btn btn-contacto">
+            Editar
+        </button>
+    </form>
+</section>
