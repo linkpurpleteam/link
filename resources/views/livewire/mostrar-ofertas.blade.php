3c148af735e791710003689a9e1d7c96c1b6a25b
linkpurpleteam <linkpurpleteam@gmail.com>
2023-11-28 19:41:26 -0500

    separacion tarjetas

--- resources/views/livewire/mostrar-ofertas.blade.php
+++ resources/views/livewire/mostrar-ofertas.blade.php
@@ -1,35 +1,32 @@
-        
 <div class="tarjetas-publi-mostrar"> 
-    @if (count ($ofertas) > 0)
-    
-    @foreach ($ofertas as $oferta )
-    <div class="card-publicacion">
-        <div>
-            <a href="{{route('oferta.show', $oferta->id)}}">{{$oferta->titulo}}</a>
-            <p>Descripcion{{$oferta->descripcion}}</p>
-        </div>   
+    @if (count($ofertas) > 0)
+        @foreach ($ofertas as $oferta)
+            <div class="card-publicacion">
+                <div class="card-content">
+                    <a href="{{ route('oferta.show', $oferta->id) }}">{{ $oferta->titulo }}</a>
+                    <p>Descripción: {{ $oferta->descripcion }}</p>
+                </div>   
 
-        <div>
-            <a href="{{route('solicitantes.index', $oferta)}}">
-                {{$oferta->solicitantes->count()}}
-                Interesados</a>
-            <a href="{{route('oferta.edit',$oferta->id)}}">Editar</a>
-            <button class="boton-eliminar" wire:click="$dispatch('mostrarAlerta', {{$oferta->id}})">
-                Eliminar</button>
-        </div>
-
-    @endforeach
-
-        @else
-        <p>No hay ofertas que Mostrar</p>
-        @endif
-
-    
+                <div class="card-actions">
+                    <a href="{{ route('solicitantes.index', $oferta) }}">
+                        {{ $oferta->solicitantes->count() }} Interesados</a>
+                    <a href="{{ route('oferta.edit', $oferta->id) }}">Editar</a>
+                    <button class="boton-eliminar" wire:click="$dispatch('mostrarAlerta', {{ $oferta->id }})">
+                        Eliminar
+                    </button>
+                </div>
+            </div>
+        @endforeach
+    @else
+        <p>No hay ofertas que mostrar</p>
         
-    </div>   
+    @endif
     <div class="livewire-pagination-container pagination">
-            {{$ofertas->links()}}
-    </div> 
+        {{ $ofertas->links() }}
+    </div>
+</div>
+
+
 </div>
 @push('scripts')
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
